22455fe9fa1a2368ff1fbe4a0f65f340c0accb3a
GalynaSimonova <simonov.galyna@gmail.com>
2018-09-18 19:15:06 +0300

    add dataservice and searchapp

---
+++ searchapp/src/main/java/com/packt/sb5be/searchapp/Controller.java
@@ -0,0 +1,20 @@
+package com.packt.sb5be.searchapp;
+
+import com.packt.sb5be.searchapp.dbmodel.orm.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    TopicRepository topicRepository;
+
+    @RequestMapping
+    public List<Topic> index() {
+        return topicRepository.findByDescriptionLikeIgnoreCase("%spring%");
+    }
+}
---
+++ searchapp/src/main/resources/searchapp_db_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO topic (name, description, text_field1, text_field2)
+    VALUES
+      ('Spring Boot vs. Spring Framework',
+        'Spring framework....',
+        ',',
+       'Spring framework on the....'
+    )
