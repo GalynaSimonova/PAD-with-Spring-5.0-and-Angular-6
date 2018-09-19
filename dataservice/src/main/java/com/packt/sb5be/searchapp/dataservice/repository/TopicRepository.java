d3302e06a0715c22f779f9a700f652611c74e258
GalynaSimonova <simonov.galyna@gmail.com>
2018-09-19 11:07:56 +0300

    Add JPA Repository Extensions

--- dataservice/src/main/java/com/packt/sb5be/searchapp/dataservice/repository/TopicRepository.java
+++ dataservice/src/main/java/com/packt/sb5be/searchapp/dataservice/repository/TopicRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface TopicRepository extends CrudRepository<Topic, Long> {
+public interface TopicRepository extends CrudRepository<Topic, Long>, ExtendedRepository<Topic, Long> {
     List<Topic> findByDescriptionLikeIgnoreCase(String description);
 }
--- dataservice/src/test/java/com/packt/sb5be/searchapp/dataservice/DataserviceApplicationTests.java
+++ dataservice/src/test/java/com/packt/sb5be/searchapp/dataservice/DataserviceApplicationTests.java
@@ -1,12 +1,17 @@
 package com.packt.sb5be.searchapp.dataservice;
 
 import com.packt.sb5be.searchapp.dataservice.dbmodel.orm.Topic;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.net.www.http.HttpClient;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -16,24 +21,55 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DataserviceApplicationTests {
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @Before
+    public void setup() {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        this.restTemplate.getRestTemplate().setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+    }
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@Test
-    public void testBasicSearch(){
-	    Topic[] topics = this.restTemplate.getForObject("/topics?searchString=spring",Topic[].class);
-	    assertTrue(topics.length >=2);
-	    for(int i = 0; i < topics.length; i++){
-	        assertTrue(topics[i].getName() != null);
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testBasicSearch() {
+        Topic[] topics = this.restTemplate.getForObject("/topics?searchString=spring", Topic[].class);
+        assertTrue(topics.length >= 2);
+        for (int i = 0; i < topics.length; i++) {
+            assertTrue(topics[i].getName() != null);
         }
     }
 
+    @Test
+    public void testBasicGet() {
+        Topic topic = doGetTopic(Long.valueOf(2));
+        assertTrue(topic.getName().contains("Spring"));
+    }
 
+    @Test
+    public void testBasicPost() {
+        Topic topic = new Topic();
+        String name = "The new topic";
+        String desc = "New topic description";
+        String tfield1 = "Text field 1";
+        String tfield2 = "Text field 2";
+        topic.setName(name);
+        topic.setDescription(desc);
+        topic.setTextField1(tfield1);
+        topic.setTextField2(tfield2);
+        Topic createdTopic = this.restTemplate.postForObject("/topics", topic, Topic.class);
+        assertTrue(createdTopic.getId() != null);
+        assertTrue(createdTopic.getName().equals(name));
+    }
 
-
+    private Topic doGetTopic(Long id) {
+        Topic topic = this.restTemplate.getForObject("/topic/" + id, Topic.class);
+        assertTrue(topic != null);
+        assertTrue(topic.getId() != null);
+        return topic;
+    }
 
 }
